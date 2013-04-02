290a490878b7d5ac6446a9628467a8bc325d18fe
Steve Robbins <srobbins@eyemaginetech.com>
2013-04-02 15:40:29 -0700

    fixing terminal issues and note behavior

--- tools.php
+++ tools.php
@@ -6,14 +6,21 @@ define("REQUEST_START", microtime(true));
 
 header("Content-Type: text/plain");
 
-array_map(create_function('$a', 'print chr($a);'), array(27, 91, 72, 27, 91, 50, 74));
+// clears terminal
+if (php_sapi_name() == 'cli') array_map(create_function('$a', 'print chr($a);'), array(27, 91, 72, 27, 91, 50, 74));
 
 function note($message = '') {
 
-    echo str_pad(round(microtime(true) - REQUEST_START, 3), 10);
+    echo str_pad((float)round(microtime(true) - REQUEST_START, 3), 10);
 
+    $canEcho = true;
     if (is_string($message) || is_numeric($message)) echo $message;
-    else echo '[' . gettype($message) . ']';
+    else {
+        echo '[' . gettype($message) . ']';
+        $canEcho = false;
+    }
 
     echo "\n";
+
+    return $canEcho;
 }
