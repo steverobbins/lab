2685e968aaf529867add3808a3f10635964c768e
Steve Robbins <srobbins@eyemaginetech.com>
2013-04-03 13:46:56 -0700

    example script

---
+++ example-soap-call.php
@@ -0,0 +1,33 @@
+<?php
+
+include '../tools.php';
+include '../soap.php';
+
+// Define Soap API urls
+$url = array(
+    1 => 'http://www.example.com/api/?wsdl',
+    2 => 'http://www.example.com/api/v2_soap?wsdl'
+);
+
+// The version for this call
+$version = 2;
+
+$soap = new MultiVersionSoapClient($url[$version], "username", "key/password", $version);
+
+// If login/url retrieve failed
+if (!$soap) exit;
+
+// Make the call
+$result = $soap->call(
+    "catalog_product.info",
+    "SKU-123",
+    0,
+    array(
+        "attributes" => array("description", "price"),
+        "additional_attributes" => array("package_id", "enable_google_checkout")
+    )
+);
+
+note("The following was returned:\n");
+// Dump results without time
+note($result, false);
--- tools.php
+++ tools.php
@@ -9,9 +9,9 @@ header("Content-Type: text/plain");
 // clears terminal
 if (php_sapi_name() == 'cli') array_map(create_function('$a', 'print chr($a);'), array(27, 91, 72, 27, 91, 50, 74));
 
-function note($message = '', $date = true) {
+function note($message = '', $time = true) {
 
-    if ($date) echo str_pad((float)round(microtime(true) - REQUEST_START, 3), 10);
+    if ($time) echo str_pad((float)round(microtime(true) - REQUEST_START, 3), 10);
 
     $canEcho = true;
     if (is_string($message) || is_numeric($message)) echo $message;
